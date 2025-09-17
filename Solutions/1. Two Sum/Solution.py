29e6b923fba08588f38ae7108c9353ef31edad95
panagiotis kotsorgios <161494302+PanagiotisKotsorgios@users.noreply.github.com>
2025-09-17 11:41:46 +0300

    Update Solution.py

--- Solutions/1. Two Sum/Solution.py
+++ Solutions/1. Two Sum/Solution.py
@@ -1,11 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        # Create a dictionary to store numbers we've seen and their indices
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Dictionary to store numbers we've seen and their indices
         num_to_index = {}
         
         for i, num in enumerate(nums):
